2983ac4d721217afa503263f5c0e8f2813cbfa18
DESKTOP-R1CNS6T\gunaw <gunawanrobi25@gmail.com>
2025-03-12 22:18:56 +0700

    Add library automapper and create DTO

---
+++ framework.DTO/ProductDTO/Responses/ResProduct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace framework.DTO.ProductDTO.Responses
+{
+    public class ResProduct
+    {
+        public long Id { get; set; }
+        public Guid Guid { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string UpdatedBy { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+    }
+}
